ae09461c67e603271be9b3ccdb87868c3e95bf11
boyet <boyet007@gmail.com>
2020-08-23 23:01:41 +0700

    #32 - Deleting a Record

--- details.php
+++ details.php
@@ -1,6 +1,19 @@
 <?php 
     include('./config/db_connect.php');
 
+    if(isset($_POST['delete'])) {
+        $id_to_delete = mysqli_real_escape_string($conn, $_POST['id_to_delete']);
+        
+        $sql = "DELETE FROM pizzas WHERE id = $id_to_delete";
+        
+        if(mysqli_query($conn, $sql)) {
+            header('Location: index.php');
+        } else {
+            echo 'query error: ' . mysqli_error($conn);
+        }
+        
+    } 
+
     // check GET request id param
     if(isset($_GET['id'])) {
         $id = mysqli_real_escape_string($conn, $_GET['id']);
@@ -29,6 +42,12 @@
         <p><?php echo date($pizza['created_at']); ?></p>
         <h5>Ingredients:</h5>
         <p><?php echo htmlspecialchars($pizza['ingredients']); ?></p>
+
+        <!-- DELETE FORM -->
+        <form action="details.php" method="POST"> 
+            <input type="hidden" name="id_to_delete" value="<?php echo $pizza['id']; ?>">
+            <input type="submit" value="Delete" name="delete" class="btn brand z-depth-0">
+        </form>
     <?php else: ?>
         <h5>No such pizza exists.</h5>
     <?php endif; ?>    
