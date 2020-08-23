b1578248a4b4e7fc0391bdd49341083e05e9282f
boyet <boyet007@gmail.com>
2020-08-23 23:07:36 +0700

    #33 - Finishing Touches

--- index.php
+++ index.php
@@ -24,6 +24,7 @@
             <?php foreach($pizzas as $pizza) : ?>
                 <div class="col s6 md3">
                     <div class="card z-depth-0">
+                        <img src="./images/pizza.png" class="pizza" alt="">
                         <div class="card-content center">
                             <h6><?php echo htmlspecialchars($pizza['title']); ?></h6>
                             <ul>
--- templates/header.php
+++ templates/header.php
@@ -12,6 +12,7 @@
         .brand { background: #cbb09c !important; }
         .brand-text { color: #cbb09c !important; }
         form { max-with: 460px; margin: 20px auto; padding: 20px; }
+        .pizza { width: 100px; margin: 40px auto -30px; display:block; position: relative; top: -30px; }
     </style>
 </head>
 <body class="yellow lighten-4">
