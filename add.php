818d43e0f3b28c856eeb0ab6b845d7e0939102dd
Philip Loa <boyet007@gmail.com>
2020-08-23 17:01:25 +0700

    #17 - Forms in PHP

---
+++ add.php
@@ -0,0 +1,25 @@
+<?php 
+    if(isset($_POST['submit'])) {
+        echo $_POST['email'];
+        echo $_POST['title'];
+        echo $_POST['ingredients'];
+    }
+
+?>
+
+<?php include('templates/header.php'); ?>
+    <section class="container grey--text">
+        <h4 class="center">Add a Pizza</h4>
+        <form action="" class="white" method="POST">
+            <label for="">Your Email:</label>
+            <input type="text" name="email" id="">
+            <label for="">Pizza Title:</label>
+            <input type="text" name="title" id="">
+            <label for="">Ingredients (comma separated):</label>
+            <input type="text" name="ingredients">
+            <div class="center">
+                <input type="submit" value="submit" name="submit" class="btn brand z-depth-0">
+            </div>
+        </form>
+    </section>
+<?php include('templates/footer.php'); ?>
---
+++ templates/footer.php
@@ -0,0 +1,6 @@
+
+    <footer class="section">
+        <div class="center grey--text">Copyright 2019 Mario Pizza</div>
+
+</body>
+</html>
