3aafb77b851dc6cba46bb3582e99ed83a9290ffb
boyet <boyet007@gmail.com>
2020-08-23 23:10:49 +0700

    #34 - Tenary Operators

---
+++ sandbox.php
@@ -0,0 +1,15 @@
+<?php 
+    $score = 100
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php tuts</title>
+</head>
+<body>
+    <p><?php echo $score > 40 ? 'high score!' : 'low score'; ?></p>
+</body>
+</html>
