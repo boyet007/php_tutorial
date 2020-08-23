c370a87358d54ba4fae3af1f36ca81371d85a253
boyet <boyet007@gmail.com>
2020-08-24 00:43:55 +0700

    #42 - Classes and Object Part 02

--- sandbox02.php
+++ sandbox02.php
@@ -4,16 +4,29 @@
         public $email;
         public $name;
 
-        public function __construct($email, $name) {
-            // $this->email = 'mari@thenetninja.co.uk';
-            // $this->name = 'mario';
+        // public function __construct($email, $name) {
+        //     // $this->email = 'mari@thenetninja.co.uk';
+        //     // $this->name = 'mario';
 
-            $this->email = $email;
-            $this->name = $name;
-        }
+        //     $this->email = $email;
+        //     $this->name = $name;
+        // }
 
         public function login() {
             echo 'Hello ' . $this->email . $this->name;
+        }
+
+        public function getName() {
+            return $this->name;
+        }
+
+        public function setName($name) {
+            if(is_string($name) && strlen($name) > 1) {
+                $this->name = $name;
+                return 'name has updated to $name';
+            } else {
+                return 'not a valid name';
+            }
         }
     }
 
@@ -21,9 +34,13 @@
     //echo $userOne->name;
     //echo $userOne->email;
 
-    $userTwo = new User('Yoshi@gmail.com', 'Yoshi');
-    echo $userTwo->name;
-    $userTwo->login();
+    // $userTwo = new User('Yoshi@gmail.com', 'Yoshi');
+    // echo $userTwo->name;
+    // $userTwo->login();
+
+    $userThree = new User();
+    echo $userThree->setName('Ucok');
+    echo $userThree->getName();
 
 
 ?>
